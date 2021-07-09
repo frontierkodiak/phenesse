13bf67fd380634308e84ced8924fd346dd725655
caleb <frontierkodiak@gmail.com>
2021-07-09 16:26:57 -0500

    additional fitdist params
    a lower bound alone didn't rectify fitting problem (i.e. negative values).. trying to feed additional params to see if these are necessary for fitdist to pick up lower bound.

--- R/weib_percentile.R
+++ R/weib_percentile.R
@@ -95,8 +95,8 @@ weib_percentile <- function(observations, percentile, iterations = 500){
       # previous create_cdf_ends()
       # weib <- fitdistrplus::fitdist(observations, distr = "weibull",
       #                               method = "mle")
-      weib <- fitdistrplus::fitdist(observations, distr = "weibull",
-                                    method = "mle", lower = 0)
+      weib <- fitdistrplus::fitdist(observations, distr = "weibull", method = "mle", start=list(shape=3, scale = 50),
+                                     lower = 0, upper = Inf)
       cdf0 <- as.numeric(weib$estimate['scale']*
                            (-log(1-0.01))^(1/weib$estimate['shape']))
       cdf100 <- as.numeric(weib$estimate['scale']*
