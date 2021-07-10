c41dcd9789099ee31a461acd87db9b5ea98ea2f6
caleb <frontierkodiak@gmail.com>
2021-07-09 19:12:51 -0500

    forgot comma
    whoops

--- R/weib_percentile.R
+++ R/weib_percentile.R
@@ -95,8 +95,7 @@ weib_percentile <- function(observations, percentile, iterations = 500){
       # previous create_cdf_ends()
       # weib <- fitdistrplus::fitdist(observations, distr = "weibull",
       #                               method = "mle")
-      weib <- fitdistrplus::fitdist(data = observations, distr = "weibull", method = "mle"
-                                     lower = 0, upper = Inf)
+      weib <- fitdistrplus::fitdist(data = observations, distr = "weibull", method = "mle", lower = 0, upper = Inf)
       cdf0 <- as.numeric(weib$estimate['scale']*
                            (-log(1-0.01))^(1/weib$estimate['shape']))
       cdf100 <- as.numeric(weib$estimate['scale']*
